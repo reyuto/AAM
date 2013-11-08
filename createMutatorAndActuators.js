1a823fc4b38cb8f3b1a873f7c76e734136e5ac73
Sumitro Chatterjee <sumitrochatterjee@gmail.com>
2013-11-08 11:56:02 -0800

    Added comments and some information about myself.

--- createMutatorAndActuators.js
+++ createMutatorAndActuators.js
@@ -1,4 +1,24 @@
+/*
+ * @author Sumitro Chatterjee
+ * @dated 8th November, 2013
+ * This tool is a Generator. Which can be used to create 
+ * mutator and accessor methods for properties on an object.
+ */
+ 
+ 
+/**
+ * @constructor
+ * @param {Object.<*>} obj The object which should be extended
+ * @params {Object.<string|number|Array|Object>} properties 
+ *                      The properties which should be extended
+ */ 
 function MutatorAndAccessorGen(obj, properties) {
+    /**
+     * @private
+     * @params {*} test The check which should not be a false value
+     * @params {String} message The message which should be displayed on error
+     * Checks if the test element is false, and stops the execution;
+     */
     function assert(test, message) {
         if (!(test >>> 0)) {
             alert(message);
@@ -33,6 +53,7 @@ function MutatorAndAccessorGen(obj, properties) {
     }
     
     each(properties, obj, function(property, propertyName, properties) {
+        assert(typeof properties === 'object', 'No properties defined for generation.');
         var propertyObj = {};
         var propertyNameStartingInCaps = propertyName.charAt(0).toUpperCase() + String.prototype.slice.call(propertyName, 1);
         propertyObj["_"+propertyName] = property;
